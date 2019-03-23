e6eb7c427d68fd206b49bcc726b2536a55fca8b2
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2019-03-23 15:54:54 +0300

    Initial implementation for ov_capture_*() functions

--- include/ov.h
+++ include/ov.h
@@ -7,12 +7,17 @@
 struct ov_device {
 	struct cha cha;
 	struct chb chb;
+	struct cha_loop loop;
 	const char* error_str;
 };
 
 int  ov_init(struct ov_device* ov);
 int  ov_open(struct ov_device* ov);
 void ov_destroy(struct ov_device* ov);
+
+int ov_capture_start(struct ov_device* ov, struct packet* packet, size_t packet_size, packet_decoder_callback callback, void* user_data);
+int ov_capture_dispatch(struct ov_device* ov, int count);
+int ov_capture_stop(struct ov_device* ov);
 
 const char* ov_get_error_string(struct ov_device* ov);
 
--- src/ov.c
+++ src/ov.c
@@ -53,6 +53,68 @@ void ov_destroy(struct ov_device* ov) {
 	cha_destroy(&ov->cha);
 }
 
+int ov_capture_start(struct ov_device* ov, struct packet* packet, size_t packet_size, packet_decoder_callback callback, void* user_data) {
+
+	if (cha_switch_fifo_mode(&ov->cha) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_cha_switch_fifo_mode;
+	}
+
+	if (cha_stop_stream(&ov->cha) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_cha_stop_stream;
+	}
+
+	if (cha_write_reg(&ov->cha, 0x402, 0x4a) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_ucfg_wdata_wr;
+	}
+
+	if (cha_write_reg(&ov->cha, 0x403, 0x80 | (0x04 & 0x3F)) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_ucfg_wcmd_wr;
+	}
+
+	if (cha_loop_init(&ov->loop, &ov->cha, packet, packet_size, callback, user_data) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_cha_loop_init;
+	}
+
+	if (cha_start_stream(&ov->cha) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		goto fail_cha_start_stream;
+	}
+
+	return 0;
+
+fail_cha_start_stream:
+fail_cha_loop_init:
+fail_ucfg_wcmd_wr:
+fail_ucfg_wdata_wr:
+fail_cha_stop_stream:
+fail_cha_switch_fifo_mode:
+	return -1;
+}
+
+int ov_capture_dispatch(struct ov_device* ov, int count) {
+	int ret = 0;
+
+	if ((ret = cha_loop_run(&ov->loop, count)) == -1) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+	}
+
+	return ret;
+}
+
+int ov_capture_stop(struct ov_device* ov) {
+	if (cha_stop_stream(&ov->cha) < 0) {
+		ov->error_str = cha_get_error_string(&ov->cha);
+		return -1;
+	}
+
+	return 0;
+}
+
 const char* ov_get_error_string(struct ov_device* ov) {
 	return ov->error_str;
 }
