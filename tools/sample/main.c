23151109306c773c9825f7a4987dcda92dd772d7
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2019-08-02 14:59:41 +0300

    Reimplement sample tool using ov_ interface

--- tools/sample/main.c
+++ tools/sample/main.c
@@ -4,17 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <ftdi.h>
-
 #include <signal.h>
 
-#include <cha.h>
-#include <chb.h>
-
-#define OV_VENDOR  0x1d50
-#define OV_PRODUCT 0x607c
-
-#define PORTB_DONE_BIT     (1 << 2)  // GPIOH2
-#define PORTB_INIT_BIT     (1 << 5)  // GPIOH5
+#include <ov.h>
 
 static void packet_handler(struct ov_packet* packet, void* data) {
 	printf("[%04x] Received %d bytes at %d:", packet->flags, packet->size, packet->timestamp);
@@ -28,113 +20,60 @@ static void sighandler(int signum) {
 }
 
 int main(int argc, char** argv) {
-	unsigned char buf[] = {0x55, 0x04, 0x01, 0x00, 0x5a};
-	unsigned char inp_buf[4096];
 	int ret;
 	enum ov_usb_speed speed = OV_LOW_SPEED;
 
 	signal(SIGINT, sighandler);
 	signal(SIGTERM, sighandler);
 
-	struct cha cha;
-	struct chb chb;
-
-	struct ftdi_version_info version;
-
-	ret = cha_init(&cha);
-	if (ret == -1) {
-		fprintf(stderr, cha_get_error_string(&cha));
-		return 1;
-	}
-
-	ret = chb_init(&chb);
-	if (ret == -1) {
-		fprintf(stderr, chb_get_error_string(&chb));
-		return 1;
-	}
-
-	ret = cha_open(&cha);
-	if (ret == -1) {
-		fprintf(stderr, cha_get_error_string(&cha));
-		return 1;
-	}
-
-	ret = chb_open(&chb);
-	if (ret == -1) {
-		fprintf(stderr, chb_get_error_string(&chb));
-		return 1;
-	}
-
-	uint8_t status;
-	ret = chb_get_status(&chb, &status);
-	if (ret == -1) {
-		fprintf(stderr, chb_get_error_string(&chb));
-		return 1;
-	}
-
-	printf("%x %d %d\n", status, status & PORTB_INIT_BIT, status & PORTB_DONE_BIT);
-
-	ret = cha_switch_fifo_mode(&cha);
-	if (ret == -1) {
-		fprintf(stderr, "cha_switch_fifo_mode %s\n", cha_get_error_string(&cha));
-		return 1;
-	}
-
-	ret = cha_stop_stream(&cha);
-	if (ret == -1) {
-		fprintf(stderr, "cha_stop_stream %s\n", cha_get_error_string(&cha));
-		return 1;
-	}
-
-	ret = cha_set_usb_speed(&cha, speed);
-	if (ret == -1) {
-		fprintf(stderr, "cha_set_usb_speed %s\n", cha_get_error_string(&cha));
-		return 1;
-	}
-
-	ret = cha_get_usb_speed(&cha, &speed);
-	if (ret == -1) {
-		fprintf(stderr, "cha_get_usb_speed %s\n", cha_get_error_string(&cha));
-		return 1;
-	}
-	printf("USB speed: %x\n", speed);
-
-	ret = cha_start_stream(&cha);
-	if (ret == -1) {
-		fprintf(stderr, "cha_start_stream %s\n", cha_get_error_string(&cha));
-		return 1;
-	}
-
+	struct ov_device* ov;
 	union {
 		struct ov_packet packet;
 		char buf[1024];
 	} p;
-	struct cha_loop cha_loop;
 
-	ret = cha_loop_init(&cha_loop, &cha, &p.packet, sizeof(p), &packet_handler, NULL);
-	if (ret == -1) {
-		fprintf(stderr, "cha_loop_init %s\n", cha_get_error_string(&cha));
+	ov = ov_new();
+	if (!ov) {
+		fprintf(stderr, "%s\n", "Cannot create ov_device handler");
 		return 1;
 	}
 
-	printf("Start looping\n");
+	ret = ov_open(ov);
+	if (ret < 0) {
+		fprintf(stderr, "%s: %s\n", "Cannot open OpenVizsla device", ov_get_error_string(ov));
 
-	ret = cha_loop_run(&cha_loop, 100);
-	if (ret == -1) {
-		fprintf(stderr, "cha_loop_run %s\n", cha_get_error_string(&cha));
+		ov_free(ov);
 		return 1;
 	}
 
-	printf("Stop looping\n");
+	ret = ov_set_usb_speed(ov, speed);
+	if (ret < 0) {
+		fprintf(stderr, "%s: %s\n", "Cannot set USB speed", ov_get_error_string(ov));
 
-	ret = cha_stop_stream(&cha);
-	if (ret == -1) {
-		fprintf(stderr, "cha_stop_stream %s\n", cha_get_error_string(&cha));
+		ov_free(ov);
 		return 1;
 	}
 
-	chb_destroy(&chb);
-	cha_destroy(&cha);
+	ret = ov_capture_start(ov, &p.packet, sizeof(p), &packet_handler, NULL);
+	if (ret < 0) {
+		fprintf(stderr, "%s: %s\n", "Cannot start capture", ov_get_error_string(ov));
+
+		ov_free(ov);
+		return 1;
+	}
+
+	ret = ov_capture_dispatch(ov, 100);
+	if (ret == -1) {
+		fprintf(stderr, "%s: %s\n", "Cannot dispatch capture", ov_get_error_string(ov));
+
+		ov_capture_stop(ov);
+		ov_free(ov);
+		return 1;
+	}
+
+	ov_capture_stop(ov);
+
+	ov_free(ov);
 
 	return 0;
 }
